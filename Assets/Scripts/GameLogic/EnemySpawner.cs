3a6afa7587a15cf78ef5b64d79c7e2a3fcd534a6
Kirill Larionov <strrranger1@gmail.com>
2023-05-12 19:38:31 +0300

    Added right spawn

--- Assets/Scripts/GameLogic/EnemySpawner.cs
+++ Assets/Scripts/GameLogic/EnemySpawner.cs
@@ -14,12 +14,13 @@ namespace GameLogic
     {
         [SerializeField] private Tank[] enemyTanksPrefab;
         [SerializeField] private Transform[] spawnPositionsLeft;
+        [SerializeField] private Transform[] spawnPositionsRight;
         [SerializeField] private MonobehLevelActivator activator;
         
         [Inject] private TimeCounter _timeCounter;
 
         private int _count = 1;
-        private WaitForSeconds _waitForSeconds = new WaitForSeconds(7f);
+        private WaitForSeconds _waitForSeconds = new WaitForSeconds(8f);
 
         private void Start()
         {
@@ -28,7 +29,7 @@ namespace GameLogic
 
         private IEnumerator Spawner()
         {
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(2.5f);
             
             while (true)
             {
@@ -45,12 +46,18 @@ namespace GameLogic
 
         private void SpawnNewEnemy()
         {
-            var tankIndex = Random.Range(0, enemyTanksPrefab.Length);
-            var spawnPositionIndex = Random.Range(0, spawnPositionsLeft.Length);
+            var tankIndexLeft = Random.Range(0, enemyTanksPrefab.Length);
+            var tankIndexRight = Random.Range(0, enemyTanksPrefab.Length);
+            var spawnPositionIndexLeft = Random.Range(0, spawnPositionsLeft.Length);
+            var spawnPositionIndexRight = Random.Range(0, spawnPositionsRight.Length);;
 
-            var tank = NightPool.Spawn(enemyTanksPrefab[tankIndex], spawnPositionsLeft[spawnPositionIndex].position, Quaternion.identity);
-            tank.Init(_timeCounter);
-            tank.GetComponent<AIDestinationSetter>().target = activator.LeftTank.transform;
+            var tankLeft = NightPool.Spawn(enemyTanksPrefab[tankIndexLeft], spawnPositionsLeft[spawnPositionIndexLeft].position, Quaternion.identity);
+            tankLeft.Init(_timeCounter);
+            tankLeft.GetComponent<AIDestinationSetter>().target = activator.LeftTank.transform;
+            
+            var tankRight = NightPool.Spawn(enemyTanksPrefab[tankIndexRight], spawnPositionsRight[spawnPositionIndexRight].position, Quaternion.identity);
+            tankRight.Init(_timeCounter);
+            tankRight.GetComponent<AIDestinationSetter>().target = activator.RightTank.transform;
         }
 
     }
