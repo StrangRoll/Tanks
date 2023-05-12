14353689c6be215a72bb9745fdd86b20949c5d7e
Kirill Larionov <strrranger1@gmail.com>
2023-05-12 03:18:22 +0300

    Added enemie's pathfinding

--- Assets/Scripts/AI/EnemyPathFinder.cs
+++ Assets/Scripts/AI/EnemyPathFinder.cs
@@ -1,9 +1,22 @@
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace AI
 {
     public class EnemyPathFinder : MonoBehaviour
     {
+        [SerializeField] private Transform target;
+        [SerializeField] private NavMeshAgent agent;
+
+        private void Awake()
+        {
+            agent.updateUpAxis = false;
+            agent.updateRotation = false;
+        }
+
+        private void Update()
+        {
+            agent.SetDestination(target.position);
+        }
     }
 }
